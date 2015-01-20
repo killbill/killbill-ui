48fe7d1876de449a0a366fde60129b6dd04aec16
stephane brossier <sbrossier@groupon.com>
2015-01-19 16:02:40 -0800

    Initial files from: > rails new killbill-ui

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KillbillUi::Application.config.secret_token = '84b2a737c718216e5d80e09242da0509a939dbb8e7ef508037397e6f195ef45e64304ecd80ded781b08fc4f753468f4846c7c8e7442ac363f8ba205ae3a6469f'
